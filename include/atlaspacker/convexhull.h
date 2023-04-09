3a34bb650b0b58620791c442bc0f86d5b0ada0e5
JCash <mwesterdahl76@gmail.com>
2023-04-09 14:47:08 +0200

    doc fix

--- include/atlaspacker/convexhull.h
+++ include/atlaspacker/convexhull.h
@@ -6,4 +6,5 @@
 #include <stdint.h>
 #include <atlaspacker/atlaspacker.h>
 
+// May return 0 if it couldn't calculate a hull (i.e. if the image is empty)
 apPosf* apConvexHullFromImage(int num_planes, uint8_t* image, int width, int height, int* num_vertices);
